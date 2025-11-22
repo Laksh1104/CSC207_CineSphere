53f3dd78c490561999f2fc50346d0527a0c70f38
Khalid-Abboushi-uoft <Khalid.abboushi@mail.utoronto.ca>
2025-11-22 10:32:28 -0500

    added this.success to store the bool

--- src/main/java/use_case/signup/SignupOutputData.java
+++ src/main/java/use_case/signup/SignupOutputData.java
@@ -1,13 +1,20 @@
 package use_case.signup;
 
 public class SignupOutputData {
-    private final String username;
 
-    public SignupOutputData(String username, Boolean bool) {
+    private final String username;
+    private final boolean success;
+
+    public SignupOutputData(String username, Boolean success) {
         this.username = username;
+        this.success = success;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public boolean isSuccess() {
+        return success;
     }
 }
