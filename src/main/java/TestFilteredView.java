2ff5f715d743afec795d7bc51c35823076c3ea05
Laksh1104 <laksh.boodnah@mail.utoronto.ca>
2025-11-27 12:09:16 -0500

    Added a TestFilteredView

--- src/main/java/TestFilteredView.java
+++ src/main/java/TestFilteredView.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+
+import data_access.TmdbMovieDataAccessObject;
+import interface_adapter.filter_movies.*;
+
+import use_case.movie_filter.*;
+
+import view.FilteredView;
+
+import javax.swing.*;
 
 public class TestFilteredView {
+
+    public static void main(String[] args) {
+
+        FilterMoviesViewModel viewModel = new FilterMoviesViewModel();
+
+        FilterMoviesDataAccessInterface api = new TmdbMovieDataAccessObject("4f21f7a70c618eee7d6d68454d8d7c83");
+
+        FilterMoviesOutputBoundary presenter = new FilterMoviesPresenter(viewModel);
+
+        FilterMoviesInputBoundary interactor = new FilterMoviesInteractor(api, presenter);
+
+        FilterMoviesController controller = new FilterMoviesController(interactor);
+
+        SwingUtilities.invokeLater(() -> {
+            FilteredView fv = new FilteredView(controller, viewModel);
+            fv.setVisible(true);
+        });
+    }
 }
