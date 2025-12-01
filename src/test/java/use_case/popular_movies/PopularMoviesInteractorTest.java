69c4258098b4e733da96aad7bd217d5d0eb9a9d0
Winnie-s27 <winniexrh@gmail.com>
2025-11-30 19:39:12 -0500

    edit

--- src/test/java/use_case/popular_movies/PopularMoviesInteractorTest.java
+++ src/test/java/use_case/popular_movies/PopularMoviesInteractorTest.java
@@ -12,26 +12,29 @@ public class PopularMoviesInteractorTest {
     // Fake DAO
     private static class FakePopularMoviesDAO implements PopularMoviesDataAccessInterface {
 
+        // when everything is normal, fake DAO would return:
         List<Movie> moviesToReturn = List.of();
         List<String> posterUrlsToReturn = List.of();
-        boolean throwOnGetPopularMovies = false;
-        List<Movie> lastMoviesPassedToGetPosterUrls;
+
+        // default: no error, so it is false here
+        boolean forceError = false;
 
         @Override
         public List<Movie> getPopularMovies() {
-            if (throwOnGetPopularMovies) {
-                throw new RuntimeException("boom");
+            // if forceError is true, meaning we have error, throw an exception
+            if (forceError) {
+                throw new RuntimeException("failed to load popular movies");
             }
             return moviesToReturn;
         }
 
         @Override
         public List<String> getPosterUrls(List<Movie> movies) {
-            lastMoviesPassedToGetPosterUrls = movies;
             return posterUrlsToReturn;
         }
     }
 
+    // fake presenter
     private static class FakePopularMoviesPresenter implements PopularMoviesOutputBoundary {
 
         PopularMoviesOutputData lastOutput;
@@ -39,61 +42,69 @@ public class PopularMoviesInteractorTest {
 
         @Override
         public void present(PopularMoviesOutputData outputData) {
-            this.lastOutput = outputData;
+            lastOutput = outputData;
+            lastError = null;
         }
 
         @Override
         public void presentError(String errorMessage) {
             this.lastError = errorMessage;
+            lastOutput = null;
         }
     }
 
+    // test the normal situation
     @Test
     void execute_success_callsPresenterWithOutputData() {
         FakePopularMoviesDAO dao = new FakePopularMoviesDAO();
         FakePopularMoviesPresenter presenter = new FakePopularMoviesPresenter();
 
+        // return
         dao.moviesToReturn = List.of();
         dao.posterUrlsToReturn = List.of("url1", "url2");
 
-        PopularMoviesInteractor interactor =
-                new PopularMoviesInteractor(dao, presenter);
-
-        PopularMoviesInputData inputData = new PopularMoviesInputData(3);
-
-        interactor.execute(inputData);
-
-        // Assert
-        assertNull(presenter.lastError, "No error should be presented");
-        assertNotNull(presenter.lastOutput, "Output data should be presented");
-
-        assertEquals(dao.posterUrlsToReturn, presenter.lastOutput.getPosterUrls());
-
-        assertEquals(List.of(), presenter.lastOutput.getFilmIds());
-
-        assertEquals(3, presenter.lastOutput.getPage());
-
-        assertSame(dao.moviesToReturn, dao.lastMoviesPassedToGetPosterUrls);
-    }
-
-    @Test
-    void execute_whenDaoThrows_callsPresenterError() {
-
-        FakePopularMoviesDAO dao = new FakePopularMoviesDAO();
-        dao.throwOnGetPopularMovies = true;
-
-        FakePopularMoviesPresenter presenter = new FakePopularMoviesPresenter();
-
-        PopularMoviesInteractor interactor =
-                new PopularMoviesInteractor(dao, presenter);
+        PopularMoviesInteractor interactor = new PopularMoviesInteractor(dao, presenter);
 
         PopularMoviesInputData inputData = new PopularMoviesInputData(1);
 
         interactor.execute(inputData);
 
-        // Assert
-        assertNull(presenter.lastOutput, "When error happens, no normal output should be presented");
-        assertNotNull(presenter.lastError, "Error message should be presented");
-        assertEquals("boom", presenter.lastError);
+        // assert, presenter would receive success output
+        assertNull(presenter.lastError); // no error
+        assertNotNull(presenter.lastOutput); // output is not null
+
+        // return should be matched
+        assertEquals(dao.posterUrlsToReturn, presenter.lastOutput.getPosterUrls()); // urls matched
+
+        // list is empty
+        assertEquals(List.of(), presenter.lastOutput.getFilmIds());
+
+        // page should be matched
+        assertEquals(1, presenter.lastOutput.getPage());
+
+        // no need movietoreturn coz presenter does not do with it
+    }
+
+    // test the error situation
+    @Test
+    void execute_whenDaoThrows_callsPresenterError() {
+        FakePopularMoviesDAO dao = new FakePopularMoviesDAO();
+        FakePopularMoviesPresenter presenter = new FakePopularMoviesPresenter();
+
+        // force error, so now it is true. when interactor calls .getPopularMovies() it would throw an exception
+        dao.forceError = true;
+
+        PopularMoviesInteractor interactor = new PopularMoviesInteractor(dao, presenter);
+
+        PopularMoviesInputData inputData = new PopularMoviesInputData(1);
+
+        interactor.execute(inputData);
+
+        // assert, success output must be null in this situation
+        assertNull(presenter.lastOutput); // error situation so no output, output is null
+
+        // assert, error message presented
+        assertNotNull(presenter.lastError); // we have error so error not null
+        assertTrue(presenter.lastError.contains("failed"));
     }
 }
