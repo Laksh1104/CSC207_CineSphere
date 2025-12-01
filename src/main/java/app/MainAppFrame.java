c42e3eeed22b43b911d30bd96a01994d304e51ba
fayyazm7 <maryam.fayyaz@mail.utoronto.ca>
2025-11-30 20:33:24 -0500

    updated app builder (includes searchfilm, popularmovies, bookmovie functionality + home button, book button)

--- src/main/java/app/MainAppFrame.java
+++ src/main/java/app/MainAppFrame.java
@@ -4,12 +4,28 @@ import javax.swing.*;
 import java.awt.*;
 
 // Data Access and Entity Factories
-import data_access.BookingMovieDataAccessObject;
-import data_access.CinemaDataAccessObject;
+import data_access.*;
 import entity.CinemaFactory;
 import entity.MovieFactory;
+import interface_adapter.BookMovie.BookMovieController;
+import interface_adapter.BookMovie.BookMoviePresenter;
+import interface_adapter.SearchFilm.SearchFilmController;
+import interface_adapter.SearchFilm.SearchFilmPresenter;
+import interface_adapter.SearchFilm.SearchFilmViewModel;
+import interface_adapter.popular_movies.PopularMoviesController;
+import interface_adapter.popular_movies.PopularMoviesPresenter;
+import interface_adapter.popular_movies.PopularMoviesViewModel;
+import use_case.book_movie.*;
 
 // Views
+import use_case.popular_movies.PopularMoviesDataAccessInterface;
+import use_case.popular_movies.PopularMoviesInputBoundary;
+import use_case.popular_movies.PopularMoviesInteractor;
+import use_case.popular_movies.PopularMoviesOutputBoundary;
+import use_case.search_film.SearchFilmDataAccessInterface;
+import use_case.search_film.SearchFilmInputBoundary;
+import use_case.search_film.SearchFilmInteractor;
+import use_case.search_film.SearchFilmOutputBoundary;
 import view.LoggedInView;
 import view.BookingView;
 import view.ScreenSwitchListener; // Assuming this interface is in the 'view' package
@@ -25,15 +41,16 @@ public class MainAppFrame extends JFrame implements ScreenSwitchListener {
 
     // View Models (Only need one for Booking in this example)
     private final BookMovieViewModel vm = new BookMovieViewModel();
+    private final PopularMoviesViewModel pmv = new PopularMoviesViewModel();
+    private final SearchFilmViewModel svm = new SearchFilmViewModel();
 
-    // 1. Instantiate the concrete DAOs and Factories
     private final BookingMovieDataAccessObject movieDAO =
             new BookingMovieDataAccessObject(new MovieFactory());
     private final CinemaDataAccessObject cinemaDAO =
             new CinemaDataAccessObject(new CinemaFactory());
-
-    // 2. Pass the instantiated DAOs to the BookingQuery
     private final BookingQuery query = new BookingQuery(movieDAO, cinemaDAO);
+    private final BookTicketDataAccessInterface ticketDAO = new InMemoryTicketDataAccessObject();
+
 
 
     private final JPanel cards;
@@ -59,13 +76,29 @@ public class MainAppFrame extends JFrame implements ScreenSwitchListener {
 
         // --- Instantiate Views ---
         LoggedInView loggedInView = new LoggedInView();
-
-        // Use the correctly initialized 'vm' and 'query'
         BookingView bookingView = new BookingView(vm, query);
-
-        // Placeholder for WatchlistView (needs its own setup and listener injection)
         JPanel watchlistView = new JPanel();
         watchlistView.add(new JLabel("Watchlist View Placeholder"));
+        MovieDataAccessInterface bookMovieDAO = this.movieDAO;
+        BookMovieOutputBoundary bookMoviePresenter = new BookMoviePresenter(vm);
+        BookMovieInputBoundary bookMovieInteractor = new BookMovieInteractor(ticketDAO, bookMoviePresenter);
+        BookMovieController bookMovieController = new BookMovieController(bookMovieInteractor);
+        bookingView.setBookMovieController(bookMovieController);
+
+        PopularMoviesDataAccessInterface popularMoviesDAO = new PopularMoviesDataAccessObject("Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYjQ3NTdjZWNmMTdjNDQyMDcyM2M0NTdhYWNkNjFlNiIsIm5iZiI6MTc2Mjc5NDA2My4xNjMsInN1YiI6IjY5MTIxYTRmMGZmMTVkYTY4NDlhYzQ3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bUPbgDcky9nR63moe3ftxhKkuEQPJ-bB0F5qmL2AUfo");
+        PopularMoviesOutputBoundary popularMoviesPresenter = new PopularMoviesPresenter(pmv);
+        PopularMoviesInputBoundary popularMoviesInteractor = new PopularMoviesInteractor(popularMoviesDAO, popularMoviesPresenter);
+        PopularMoviesController popularMoviesController = new PopularMoviesController(popularMoviesInteractor);
+        loggedInView.setPopularMoviesDependencies(popularMoviesController, pmv);
+
+        SearchFilmDataAccessInterface searchFilmDAO = new SearchFilmDataAccessObject();
+        SearchFilmOutputBoundary searchFilmPresenter = new SearchFilmPresenter(svm);
+        SearchFilmInputBoundary searchFilmInteractor = new SearchFilmInteractor(searchFilmDAO, searchFilmPresenter);
+        SearchFilmController searchFilmController = new SearchFilmController(searchFilmInteractor);
+        loggedInView.setSearchDependencies(searchFilmController, svm);
+
+        loggedInView.setMovieDetailsDependencies();
+
 
 
         // --- Essential Step: Inject the Listener ---
