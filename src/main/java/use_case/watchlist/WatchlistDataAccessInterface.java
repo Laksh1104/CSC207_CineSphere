3522e58464a319e9b4c5dc7ffcda2b24e3285b18
Khalid-Abboushi-uoft <Khalid.abboushi@mail.utoronto.ca>
2025-12-01 20:12:13 -0500

    use case changed

--- src/main/java/use_case/watchlist/WatchlistDataAccessInterface.java
+++ src/main/java/use_case/watchlist/WatchlistDataAccessInterface.java
@@ -2,6 +2,11 @@ package use_case.watchlist;
 
 import java.util.List;
 
+/**
+ * Data access interface for persisting and loading a user's watchlist.
+ *
+ * Implemented by an outer-layer gateway (e.g. UserProfileJsonDataAccessObject).
+ */
 public interface WatchlistDataAccessInterface {
 
     boolean isInWatchlist(String username, String posterUrl);
@@ -10,5 +15,11 @@ public interface WatchlistDataAccessInterface {
 
     void removeFromWatchlist(String username, String posterUrl);
 
+    /**
+     * Returns all poster URLs for the given user.
+     *
+     * @param username current logged-in username
+     * @return list of poster URLs (never null)
+     */
     List<String> getWatchlist(String username);
 }
--- src/main/java/use_case/watchlist/WatchlistOutputBoundary.java
+++ src/main/java/use_case/watchlist/WatchlistOutputBoundary.java
@@ -1,6 +1,13 @@
 package use_case.watchlist;
 
-
+/**
+ * Output boundary for the Watchlist use case.
+ *
+ * Implemented by a presenter in the interface_adapter layer.
+ */
 public interface WatchlistOutputBoundary {
+
     void present(WatchlistOutputData data);
+
+    void presentError(String errorMessage);
 }
